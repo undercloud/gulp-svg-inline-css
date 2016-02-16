bc89c0dad1997369b04a2f0ec5e975325783f8a0
mooden <alexandrostruses@yandex.ru>
2016-02-16 23:55:29 +0400

    new features

--- index.js
+++ index.js
@@ -12,10 +12,28 @@ var through = require('through2'),
 _.trim = require('underscore.string/trim');
 _.sprintf = require('underscore.string/sprintf');
 
+if(typeof Object.isset != 'function'){
+	Object.isset = function(ns, g){
+		var chain = ns.split('.'),
+			root = g || scope;
+
+		for(var i=0, l=chain.length; i<l; i++){
+			if(typeof root[chain[i]] === "undefined"){
+				return false;
+			}
+
+			root = root[chain[i]];
+		}
+
+		return true;
+	};
+}
+
 module.exports = function(options) {
 	options = options || {};
 	options.style = options.style || {};
 	options.className = options.className || '.icon.%s';
+	options.raw = options.raw || false;
 
 	return through.obj(function(file, encoding, done) {
 
@@ -46,11 +64,26 @@ module.exports = function(options) {
 
 			data = svg.read(data);
 
+			if (Object.isset('width', options)) {
+				if (Object.isset('svg.$', data)) {
+					data.svg.$.width = options.width;
+				}
+			}
+
+			if (Object.isset('height', options)) {
+				if (Object.isset('svg.$', data)) {
+					data.svg.$.height = options.height;
+				}
+			}
+
 			content = builder.buildObject(data);
-			content = svg.buildContent(file, options, content);
+
+			if (options.raw === false) {
+				content = svg.buildContent(file, options, content);
+				file.path = svg.genPath(file);
+			}
 
 			file.contents = new Buffer(content);
-			file.path = svg.genPath(file);
 
 			return done(null, file);
 		});
